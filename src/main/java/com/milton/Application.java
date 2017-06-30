460e383f673d315c543c59502a76fd24dc99353e
zhaobingxtu@163.com <13989584413>
2017-06-30 09:29:10 +0800

    增加向spring cloud注册服务

--- src/main/java/com/milton/Application.java
+++ src/main/java/com/milton/Application.java
@@ -5,11 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @MapperScan("com.milton.dao")
 @ServletComponentScan
 @EnableCaching
+@EnableDiscoveryClient
 public class Application {
 
 	public static void main(String[] args) {
